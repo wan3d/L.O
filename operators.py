77008c0aa6dba7d80c2c436fe2329bf00e91dd34
Zullo Jean Piero <128077087+wan3d@users.noreply.github.com>
2026-01-29 00:55:35 -0600

    Compiler first steps.

---
+++ operators.py
@@ -0,0 +1,31 @@
+# AND
+def AND(a, b):
+    return a & b
+
+# OR 
+def OR(a, b):
+    return a | b
+# NOT 
+def NOT(a):
+    return not a
+
+# XOR 
+def XOR(a, b):
+    return a ^ b 
+
+# BUFFER 
+def BUFFER(a):
+    return a
+
+# NAND 
+def NAND(a, b):
+    return not a & b
+
+# NOR 
+def NOR(a, b):
+    return not a | b
+
+# XNOR 
+def XNOR(a, b):
+    return not a ^ b
+
